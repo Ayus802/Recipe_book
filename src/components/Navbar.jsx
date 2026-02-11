c4eb9401fab283664a970468210a3e76b931f7dd
Ayush Kumar Gupta <guptayush47@gmail.com>
2026-02-11 19:48:50 +0530

    Fix : UI updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,9 @@ const Navbar = () => {
     <motion.nav
       initial={{ y: -50 }}
       animate={{ y: 0 }}
-      className="bg-white/70 backdrop-blur-md shadow-md p-4 flex justify-between items-center"
+      className="bg-orange-400 backdrop-blur-md shadow-md p-4 flex justify-between items-center"
     >
-      <Link to="/" className="text-2xl font-bold text-orange-500 align-middle">
+      <Link to="/" className="text-2xl font-bold text-white align-middle">
         <img src="https://img.freepik.com/premium-vector/eat-food-restaurant-icon-orange-color-design_1178600-1070.jpg" alt="" className="size-9 inline mr-2"/> Recipe Book
       </Link>
     </motion.nav>
